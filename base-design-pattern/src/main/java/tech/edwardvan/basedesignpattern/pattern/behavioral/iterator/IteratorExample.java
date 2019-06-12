f6424f0523389bb09cd7ef6f7de01497700b21ac
EdwardVan <909008365@qq.com>
2019-06-12 21:33:01 +0800

    设计模式 迭代器模式

---
+++ base-design-pattern/src/main/java/tech/edwardvan/basedesignpattern/pattern/behavioral/iterator/IteratorExample.java
@@ -0,0 +1,77 @@
+package tech.edwardvan.basedesignpattern.pattern.behavioral.iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * 迭代器模式
+ *
+ * @author EdwardVan
+ */
+public class IteratorExample {
+
+    public static void main(String[] args) {
+        ConcreteContainer concreteContainer = new ConcreteContainer();
+        concreteContainer.add(1);
+        concreteContainer.add(2);
+        concreteContainer.add(3);
+        Iterator iterator = concreteContainer.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+
+    /**
+     * 具体的容器
+     */
+    public static class ConcreteContainer {
+
+        private List list;
+
+        public ConcreteContainer() {
+            list = new ArrayList();
+        }
+
+        public void add(Object obj) {
+            list.add(obj);
+        }
+
+        public Iterator iterator() {
+            return new ConcreteIterator();
+        }
+
+        public Object get(int index) {
+            return list.get(index);
+        }
+
+        public int getSize() {
+            return list.size();
+        }
+
+        /**
+         * 具体的迭代器
+         */
+        public class ConcreteIterator implements Iterator {
+
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                if (index >= list.size()) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+
+            @Override
+            public Object next() {
+                Object object = list.get(index);
+                index++;
+                return object;
+            }
+        }
+
+    }
+}

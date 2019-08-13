5004568ad18eb0e5c98c3cf4f0314eadd9250890
EdwardVan <909008365@qq.com>
2019-08-13 18:35:51 +0800

    消息队列

---
+++ web-spring-boot/src/main/java/tech/edwardvan/webspringboot/config/RabbitConfig.java
@@ -0,0 +1,19 @@
+package tech.edwardvan.webspringboot.config;
+
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RabbitMQ配置类
+ *
+ * @author EdwardVan
+ */
+@Configuration
+public class RabbitConfig {
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+}

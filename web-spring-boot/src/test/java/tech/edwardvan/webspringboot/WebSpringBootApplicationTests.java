6f62e2bf3134c7897cf1cfd9f30ca5592e75c5f4
EdwardVan <909008365@qq.com>
2019-08-26 22:29:24 +0800

    elasticsearch

--- web-spring-boot/src/test/java/tech/edwardvan/webspringboot/WebSpringBootApplicationTests.java
+++ web-spring-boot/src/test/java/tech/edwardvan/webspringboot/WebSpringBootApplicationTests.java
@@ -1,5 +1,10 @@
 package tech.edwardvan.webspringboot;
 
+import io.searchbox.client.JestClient;
+import io.searchbox.core.DocumentResult;
+import io.searchbox.core.Index;
+import io.searchbox.core.Search;
+import io.searchbox.core.SearchResult;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.amqp.core.Message;
@@ -10,7 +15,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import tech.edwardvan.webspringboot.config.TestConfiguration;
+import tech.edwardvan.webspringboot.model.User;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 @RunWith(SpringRunner.class)
@@ -29,8 +36,35 @@ public class WebSpringBootApplicationTests {
     @Autowired
     RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    JestClient jestClient;
+
     @Test
     public void contextLoads() {
+    }
+
+    @Test
+    public void testElasticsearch() throws IOException {
+        User user = new User();
+        user.setUsername("Edward");
+        user.setPassword("123456");
+        user.setEmail("test@qq.com");
+
+        Index indexBuild = new Index.Builder(user).index("test").type("user").id("1").build();
+        DocumentResult indexBuildResult = jestClient.execute(indexBuild);
+        System.out.println("jestClient:"+indexBuildResult.getId());
+
+        String queryJson = "{\n" +
+                "    \"query\" : {\n" +
+                "        \"match\" : {\n" +
+                "            \"username\" : \"Edward\"\n" +
+                "        }\n" +
+                "    }\n" +
+                "}";
+
+        Search searchBuild = new Search.Builder(queryJson).addIndex("test").addType("user").build();
+        SearchResult searchBuildResult = jestClient.execute(searchBuild);
+        System.out.println("jestClient:"+searchBuildResult.getJsonString());
     }
 
 }

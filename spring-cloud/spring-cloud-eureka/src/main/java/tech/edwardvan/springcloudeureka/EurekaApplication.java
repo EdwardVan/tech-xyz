3c67972c4c840bf4522db7805625389dadc3b041
EdwardVan <909008365@qq.com>
2019-09-18 16:48:46 +0800

    spring cloud eureka集群搭建

---
+++ spring-cloud/spring-cloud-eureka/src/main/java/tech/edwardvan/springcloudeureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package tech.edwardvan.springcloudeureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka注册中心启动类
+ *
+ * @author EdwardVan
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}

9106a24f4177cc06ea127724ffbcfecc6636812e
EdwardVan <909008365@qq.com>
2019-09-20 18:26:45 +0800

    spring cloud 服务发现注册

--- spring-cloud/spring-cloud-user/spring-cloud-user-server/src/main/java/tech/edwardvan/springclouduserserver/UserApplication.java
+++ spring-cloud/spring-cloud-user/spring-cloud-user-server/src/main/java/tech/edwardvan/springclouduserserver/UserApplication.java
@@ -4,12 +4,14 @@ package tech.edwardvan.springclouduserserver;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author EdwardVan
  */
 @SpringBootApplication
 @MapperScan("tech.edwardvan.springclouduserserver.dao")
+@EnableEurekaClient
 public class UserApplication {
 
     public static void main(String[] args) {

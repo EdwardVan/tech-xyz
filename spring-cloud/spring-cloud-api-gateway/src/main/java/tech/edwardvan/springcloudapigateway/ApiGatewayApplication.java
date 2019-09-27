2ecb1699b7e841af09d3cdc8999b9dd59a26775d
EdwardVan <909008365@qq.com>
2019-09-27 20:34:01 +0800

    spring cloud zuul

---
+++ spring-cloud/spring-cloud-api-gateway/src/main/java/tech/edwardvan/springcloudapigateway/ApiGatewayApplication.java
@@ -0,0 +1,21 @@
+package tech.edwardvan.springcloudapigateway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 服务网关
+ *
+ * @author EdwardVan
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}

1dee582fb1bc8d5549076773404d71f685f083cb
EdwardVan <909008365@qq.com>
2019-09-30 15:52:22 +0800

    spring cloud config 高可用

--- spring-cloud/spring-cloud-eureka/src/main/java/tech/edwardvan/springcloudeureka/EurekaApplication.java
+++ spring-cloud/spring-cloud-eureka/src/main/java/tech/edwardvan/springcloudeureka/EurekaApplication.java
@@ -7,10 +7,14 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Eureka注册中心启动类
- *
+ * <p>
+ * hosts添加参数:
+ * 127.0.0.1 eureka-peer1 eureka-peer2
+ * <p>
  * Idea启动参数:
  * --server.port=8762 --eureka.instance.hostname=eureka-peer2 --eureka.client.service-url.defaultZone=http://eureka-peer1:8761/eureka/
  * --server.port=8761 --eureka.instance.hostname=eureka-peer1 --eureka.client.service-url.defaultZone=http://eureka-peer2:8762/eureka/
+ *
  * @author EdwardVan
  */
 @SpringBootApplication

6d006ddf0c4ce0a87263f148bf4ad87b0c5f95c8
EdwardVan <909008365@qq.com>
2019-12-03 02:15:19 +0800

    项目依赖版本升级

---
+++ rbac/rbac-spring-security/rbac-spring-security-demo/src/main/java/tech/edwardvan/rbacspringsecuritydemo/RbacSpringSecurityDemoApplication.java
@@ -0,0 +1,14 @@
+package tech.edwardvan.rbacspringsecuritydemo;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class RbacSpringSecurityDemoApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

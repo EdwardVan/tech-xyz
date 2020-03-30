b2710eae104331387080be7da3b495c796e9399d
EdwardVan <909008365@qq.com>
2020-03-31 01:59:34 +0800

    工具 mybatis plus 代码生成器

--- os-mybatis-plus/src/main/java/tech/edwardvan/osmybatisplus/MyBatisPlusApplication.java
+++ os-mybatis-plus/src/main/java/tech/edwardvan/osmybatisplus/MyBatisPlusApplication.java
@@ -1,4 +1,16 @@
 package tech.edwardvan.osmybatisplus;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author EdwardVan
+ */
+@SpringBootApplication
+@MapperScan("tech.edwardvan.osmybatisplus.dao")
 public class MyBatisPlusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisPlusApplication.class, args);
+    }
 }

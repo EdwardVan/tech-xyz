9046837f96e1ef4bad134bafa5393675402b81b0
EdwardVan <909008365@qq.com>
2020-04-07 00:52:36 +0800

    spring cloud alibaba

--- ms-spring-cloud/ms-spring-cloud-product/ms-spring-cloud-product-server/src/main/java/tech/edwardvan/msspringcloudproductserver/ProductApplication.java
+++ ms-spring-cloud/ms-spring-cloud-product/ms-spring-cloud-product-server/src/main/java/tech/edwardvan/msspringcloudproductserver/ProductApplication.java
@@ -3,15 +3,17 @@ package tech.edwardvan.msspringcloudproductserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Idea启动参数:
- * --server.port=8081
- * --server.port=8082
+ * --server.port=8091
+ * --server.port=8092
  *
  * @author EdwardVan
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);

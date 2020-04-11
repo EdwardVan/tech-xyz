b2daa47fe8e930299a87aa4ce8594c41990a9e06
EdwardVan <909008365@qq.com>
2020-04-11 19:09:07 +0800

    spring cloud gateway 自定义过滤器

---
+++ ms-spring-cloud/ms-spring-cloud-gateway/src/main/java/tech/edwardvan/msspringcloudgateway/filter/TestGatewayFilter.java
@@ -0,0 +1,28 @@
+package tech.edwardvan.msspringcloudgateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 自定义过滤器
+ *
+ * @author EdwardVan
+ */
+@Component
+@Order(0)
+@Slf4j
+public class TestGatewayFilter implements GlobalFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        log.warn("RemoteAddress:{}", request.getRemoteAddress());
+        return chain.filter(exchange);
+    }
+}

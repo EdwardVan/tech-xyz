590de0d4655fdb2e8aa42b5a1fd2e40f629d9f69
EdwardVan <909008365@qq.com>
2020-10-19 18:16:06 +0800

    更新pom

---
+++ mall-mono/mall-mono-api/src/main/java/tech/edwardvan/mallmonoapi/MallMonoApiApplication.java
@@ -0,0 +1,14 @@
+package tech.edwardvan.mallmonoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author EdwardVan
+ */
+@SpringBootApplication
+public class MallMonoApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallMonoApiApplication.class, args);
+    }
+}
